f098227328b8fe863e7305184cfe6a8528b40c30
Thomas Sankara <thomassankara74@gmail.com>
2022-06-06 14:18:41 +0300

    timeline

---
+++ src/components/Timeline.js
@@ -0,0 +1,82 @@
+export default function Timeline() {
+  return (
+    <>
+      <div className="timeline py-20" id="timeline" name="timeline">
+        <div className="wrapper left">
+          <div className="content">
+            <h2>Receptionist</h2>
+            <ul className="text-sm mb-5">
+              <li className="mb-3">
+                Kwops Engineering and Construction, Kitengela, Kenya
+              </li>
+              <li>November 2021 - March 2022</li>
+            </ul>
+            <p>
+              Worked as a receptionist at Kwops Engineering and Construction
+              Company in Kitengela.
+            </p>
+          </div>
+        </div>
+
+        <div className="wrapper right">
+          <div className="content">
+            <h2>HOUSEKEEPER AND SHOP ATTENDANT</h2>
+            <ul className="text-sm mb-5">
+              <li className="mb-3">Sideview Apartment, Nairobi, Kenya</li>
+              <li>February 2021 - November 2021</li>
+            </ul>
+            <p>
+              Cleaning experience in Sideview apartment as well as a shop
+              attendant at the same place.
+            </p>
+          </div>
+        </div>
+
+        <div className="wrapper left">
+          <div className="content">
+            <h2>MASSAGE TRAINEE</h2>
+            <ul className="text-sm mb-5">
+              <li className="mb-3">H&amp;H Wellness, Westlands, Kenya</li>
+              <li>December 2020 - February 2021</li>
+            </ul>
+            <p>
+              Massage trainee and a part-time worker as a cleaner at H&amp;H
+              Wellness in Westlands
+            </p>
+          </div>
+        </div>
+
+        <div className="wrapper right">
+          <div className="content">
+            <h2>SUPERVISOR</h2>
+            <ul className="text-sm mb-5">
+              <li className="mb-3">Apartment Construction, Donholm, Kenya</li>
+              <li>November 2019 - March 2020</li>
+            </ul>
+            <p>
+              Supervisor at an apartment construction in Donholm under Engineer
+              Hillary
+            </p>
+          </div>
+        </div>
+
+        <div className="wrapper left">
+          <div className="content">
+            <h2>ATTACHE</h2>
+            <ul className="text-sm mb-5">
+              <li className="mb-3">
+                Kenya Rural Roads Authority, Ainabkoi, Kenya
+              </li>
+              <li>September 2017 - December 2017</li>
+            </ul>
+            <p>
+              I was an attaché at Kenya Rural Roads Authority at an Improvement
+              of Bitumen Standard and Maintenance of Dry’s – Ainabkoi –
+              Kapchorwa – Naiberi Road, September to November 2017
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
