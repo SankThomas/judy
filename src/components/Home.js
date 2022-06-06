d5763e87e54f5e5d27e64fe0b860a80dcedfd334
Thomas Sankara <thomassankara74@gmail.com>
2022-06-06 14:19:02 +0300

    update component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
+import ContactForm from "./ContactForm"
+import Footer from "./Footer"
 import Showcase from "./Showcase"
+import Timeline from "./Timeline"
 
 export default function Home() {
   return (
     <>
       <Showcase />
+      <Timeline />
+      <ContactForm />
+      <Footer />
     </>
   )
 }
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -4,18 +4,21 @@ export default function Showcase() {
   return (
     <>
       <section className="showcase">
-        <div className="overlay grid grid-cols-1 place-content-center">
-          <article className="max-width">
-            <h1 className="mb-5 heading-1">
-              Civil engineering, home decor <br /> and architecture
-            </h1>
-            <p className="mb-10 text-slate-300 md:w-1/2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-              voluptas enim voluptatum. Vitae nostrum voluptatibus rerum
-              laudantium consequatur ut perferendis est repellat sint molestiae
-            </p>
-            <button className="btn-primary">Contact Us</button>
-          </article>
+        <div className="overlay">
+          <div className="max-width grid grid-cols-1 place-items-center h-screen">
+            <article className="text-center">
+              <h1 className="mb-5 heading-1">Civil Engineer, Home Decorator</h1>
+              <p className="mb-10 text-slate-300">
+                I advise, coordinate and supervize the technical aspects of road
+                and building construction projects.
+              </p>
+              <a href="#timeline" className="btn-primary">
+                View Work Timeline
+              </a>
+            </article>
+
+            {/* <article></article> */}
+          </div>
         </div>
       </section>
     </>
